0c21b1ba730b9d13090fbf6e756919148f65ef13
Katazui <Katazui@live.com>
2021-03-22 13:32:32 -0400

    Added - Question Type & Question Limit
    Working on Sound Effects

--- Chess/ChessColorQuiz/ChessColorQuiz.java
+++ Chess/ChessColorQuiz/ChessColorQuiz.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 import java.util.Random;
-public class ChessDemo {
-   public static void main(String[] args) {
-      Scanner keyboard = new Scanner(System.in);
+public class ChessColorQuiz {
+   public static void main(String[] args) throws Exception {  
+      //Creating Chessboard Array
       String[][] chessBoard = new String[64][64];
-      Random random = new Random();
       int number = 1;
       for(int i = 0; i < 64; i++) {
          if ((i >= 0) && (i < 8)) {
@@ -80,39 +79,71 @@ public class ChessDemo {
          }
          number++;
       }
+      //Display Chessboard Array
       int lineCut = 0;
+      int lineCutTwice = 0;
       for(int i = 0; i < 64; i++) {
          System.out.print(chessBoard[i][0] + "-" + chessBoard[i][1] + " | ");
          lineCut++;
+         lineCutTwice++;
+         if(lineCutTwice == 32) { System.out.println(""); }
          if(lineCut == 8) {
             System.out.println("");
             lineCut = 0;
          }
       }
       System.out.println("");
+      //Chessboard Color Quiz
       int option;
       int correct = 0;
       int numberOfQuestions = 0;
+      int question;
+      Random random = new Random();
+      /*
+      String correctSoundEffect = "/Correct.mp3";
+      String incorrectSoundEffect = "/Incorrect.mp3";
+      InputStream correctInput = new FileInputStream(correctSoundEffect);
+      InputStream incorrectInput = new FileInputStream(incorrectSoundEffect);
+      */
+      Scanner keyboard = new Scanner(System.in);
+      System.out.println("ALL = 1 | TOP HALF = 2 | BOTTOM HALF = 3\n");
+      int questionType = keyboard.nextInt();
+      System.out.println("\nQuestion Limit? INFINITY <= 0\n");
+      int questionLimit = keyboard.nextInt();
       do {
-         int question = random.nextInt(64);
-         System.out.println("What color is " + chessBoard[question][0] + "? | WHITE = 1 | BLACK = 2 | SCORE = 3 | EXIT = 4 |");
+         if(questionLimit == numberOfQuestions) {
+            System.out.println("Keep playing chess! See ya later!");
+            System.out.println("\nScore: "+ correct + "/" + numberOfQuestions + "\n");
+            break;
+         }
+         if(questionType == 1) {
+             question = random.nextInt(64);
+         } else if(questionType == 2) {
+            question = random.nextInt(32);
+         } else if(questionType == 3) {
+            question = random.nextInt(32);
+            question = question + 32;
+         } else {
+            question = random.nextInt(64);
+         }
+         System.out.println("\nWhat color is " + chessBoard[question][0] + "? | WHITE = 1 | BLACK = 2 | SCORE = 3 | EXIT = 4 |");
          option = keyboard.nextInt();
          if(option == 1) {
             if ("White" == chessBoard[question][1]) {
-               System.out.println("\nCorrect!\n");
+               System.out.println("\nCorrect!\n" + chessBoard[question][0] + "-" + chessBoard[question][1] + "\n");
                correct++;
                numberOfQuestions++;
             } else {
-               System.out.println("\nIncorrect!\n");
+               System.out.println("\nIncorrect!\n" + chessBoard[question][0] + "-" + chessBoard[question][1] + "\n");
                numberOfQuestions++;
             }
          } else if(option == 2) {
             if ("Black" == chessBoard[question][1]) {
-               System.out.println("\nCorrect!\n");
+               System.out.println("\nCorrect!\n" + chessBoard[question][0] + "-" + chessBoard[question][1] + "\n");
                correct++;
                numberOfQuestions++;
             } else {
-               System.out.println("\nIncorrect!\n");
+               System.out.println("\nIncorrect!\n" + chessBoard[question][0] + "-" + chessBoard[question][1] + "\n");
                numberOfQuestions++;
             }
          } else if(option == 3) {
@@ -125,6 +156,5 @@ public class ChessDemo {
          }
          keyboard.nextLine();
       } while(option != 4);
-
    }
 }
